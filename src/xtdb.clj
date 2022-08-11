ac87bd74aa85456052ffd6ace01d49f5ef2fa1f9
FiVo <finn.volkel@gmail.com>
2022-08-11 10:03:59 +0200

    xtdb cleanup

--- src/xtdb.clj
+++ src/xtdb.clj
@@ -1,6 +1,5 @@
-(ns xt
+(ns xtdb
   (:require [clojure.java.io :as io]
-            [clojure.math.combinatorics :as combo]
             [graph]
             [xtdb.api :as xt]))
 
@@ -14,20 +13,19 @@
       :xtdb/document-store (kv-store "data/doc-store")
       :xtdb/index-store (kv-store "data/index-store")})))
 
-(def xtdb-node (start-xtdb!))
+(comment
+  (def xtdb-node (start-xtdb!)))
 
 (defn graph->docs [edges]
   (->> edges
        (reduce (fn [graph [i j]] (-> graph (update i (fnil conj #{}) j) (update j (fnil identity #{})))) {})
        (map (fn [[i neighbours]] {:xt/id i :g/to neighbours}))))
 
-(comment (graph->docs (graph/complete-graph 5) ))
+(comment
+  (graph->docs (graph/complete-graph 5)))
 
 (defn wrap-in-puts [docs]
   (map #(vector ::xt/put %) docs))
-
-(comment
-  (graph->docs (graph/complete-graph 3)))
 
 (def mem-node (xt/start-node {}))
 (comment
@@ -46,8 +44,6 @@
   (.close mem-node)
   )
 
-
-
 (def triangleq '{:find [?a ?b ?c]
                  :where [[?a :g/to ?b]
                          [?a :g/to ?c]
@@ -59,7 +55,8 @@
       (recur (into res (map #(vector (list '!= (first vars) %)) (nthrest vars 2))) (rest vars))
       res)))
 
-(no-repetition (range 4))
+(comment
+  (no-repetition (range 4)))
 
 (defn k-path-query [k]
   (let [vars (vec (repeatedly (inc k) #(symbol (str "?" (gensym)))))]
@@ -68,55 +65,43 @@
         (update :where (comp vec concat) (no-repetition vars)))))
 
 (comment
-  (k-path-query 4)
+  (k-path-query 4))
 
-  )
+(defn create-and-query [graph query]
+  (with-open [node (xt/start-node {})]
+    (xt/submit-tx node (->> graph graph->docs wrap-in-puts))
+    (xt/sync node)
+    (time (xt/q (xt/db node) query))))
 
 (comment
   (time (xt/q (xt/db mem-node) (assoc triangleq :timeout 120000)))
-
-  (xt/sync mem-node)
   (time (xt/q (xt/db mem-node) (assoc (k-path-query 4) :timeout 120000)))
 
-  (count *1)
-  )
-
-
-
-(comment
-  (def data [{:xt/id 1 :g/to #{2 3}} {:xt/id 2 :g/to #{3}} {:xt/id 3 :g/to #{}}])
-
-  (xt/submit-tx mem-node (wrap-in-puts data) )
-
-  (xt/submit-tx mem-node [[::xt/put {:xt/id 2 :g/to #{3}}]])
-
-  (xt/q (xt/db mem-node) triangle-other)
-
-  (xt/q (xt/db mem-node) '{:find [?a ?b]
-                           :where [[?a :g/to ?b]
-                                   [?a :g/to ?c]
-                                   #_[?b :g/to ?c]]})
-
-
-
+  (create-and-query (graph/complete-independents 100 7) (k-path-query 3))
 
   )
+
+;;///////////////////////////////////////////////////////////////////////////////
+;;===============================================================================
+;;                                 old approach
+;;===============================================================================
+;;\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+;; some queries below took quite a while to execute, maybe worth investigating
 
 (defn graph->docs2 [graph]
   (map (fn [[i j]] {:xt/id (str i "-" j) :g/from i :g/to j}) graph))
 
-
 (comment
-  (xt/submit-tx mem-node (->> (graph/complete-graph 200) graph->docs wrap-in-puts))
-  (xt/submit-tx mem-node (->> (graph/complete-bipartite 200) graph->docs wrap-in-puts))
-  (xt/submit-tx mem-node (->> (graph/star-graph 1000) graph->docs wrap-in-puts))
-  (xt/submit-tx mem-node (->> (graph/star-with-ring 1000) graph->docs wrap-in-puts))
+  (xt/submit-tx mem-node (->> (graph/complete-graph 200) graph->docs2 wrap-in-puts))
+  (xt/submit-tx mem-node (->> (graph/complete-bipartite 200) graph->docs2 wrap-in-puts))
+  (xt/submit-tx mem-node (->> (graph/star-graph 1000) graph->docs2 wrap-in-puts))
+  (xt/submit-tx mem-node (->> (graph/star-with-ring 1000) graph->docs2 wrap-in-puts))
 
   (require '[postgres])
 
-  (xt/submit-tx mem-node (->> postgres/rand-g graph->docs wrap-in-puts))
-  (xt/submit-tx mem-node (->> (graph/complete-independents 300 10) graph->docs wrap-in-puts))
-
+  (xt/submit-tx mem-node (->> postgres/rand-g graph->docs2 wrap-in-puts))
+  (xt/submit-tx mem-node (->> (graph/complete-independents 300 10) graph->docs2 wrap-in-puts))
 
   )
 
